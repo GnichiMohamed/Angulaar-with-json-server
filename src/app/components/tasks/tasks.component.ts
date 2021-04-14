f138ead75f33f23765981edcfd59afe336aedb7f
Mohamed Gnichi <gnichimohamed@gmail.com>
2021-04-14 15:36:55 +0100

    first commit

---
+++ src/app/components/tasks/tasks.component.ts
@@ -0,0 +1,82 @@
+import { Component, OnInit } from '@angular/core';
+import { Task } from 'src/app/models/task';
+import { TaskService } from 'src/app/services/task.service';
+
+@Component({
+  selector: 'app-tasks',
+  templateUrl: './tasks.component.html',
+  styleUrls: ['./tasks.component.css'],
+})
+export class TasksComponent implements OnInit {
+  searchText = '';
+
+  showForm = false;
+
+  editForm = false;
+
+  myTask: Task = {
+    label: '',
+    completed: false,
+  };
+
+  tasks: Task[] = [];
+  resultTasks: Task[] = [];
+
+  constructor(private taskService: TaskService) {}
+
+  ngOnInit() {
+    this.getTasks();
+  }
+
+  getTasks() {
+    this.taskService.findAll().subscribe((tasks) => {
+      this.resultTasks = this.tasks = tasks;
+    });
+  }
+
+  deleteTask(id: number | undefined) {
+    this.taskService.delete(id).subscribe(() => {
+      this.tasks = this.tasks.filter((task) => task.id != id);
+    });
+  }
+
+  persistTask() {
+    this.taskService.persist(this.myTask).subscribe((task) => {
+      // "task" here is the data returned from the api
+      this.tasks = [task, ...this.tasks]; // we sort tesk in the top of table
+      this.resetTask();
+      this.showForm = false;
+    });
+  }
+
+  resetTask() {
+    this.myTask = {
+      label: '',
+      completed: false,
+    };
+  }
+
+  toggleCompleted(task: Task) {
+    this.taskService.completed(task.id, task.completed).subscribe(() => {
+      task.completed = !task.completed;
+    });
+  }
+
+  editTask(task: Task) {
+    this.myTask = task;
+    this.editForm = true;
+  }
+
+  updateTask() {
+    this.taskService.update(this.myTask).subscribe((task) => {
+      this.resetTask();
+      this.editForm = false;
+    });
+  }
+
+  searchTasks() {
+    this.resultTasks = this.tasks.filter((task) =>
+      task.label.toLowerCase().includes(this.searchText.toLowerCase())
+    );
+  }
+}
